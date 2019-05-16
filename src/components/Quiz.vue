d0179286a1bd535914976a9e5a42fe2a8bd21b52
Mark Barton <markbarton@marks-mac-mini.home>
2019-05-16 14:15:15 +0100

    lesson9

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -8,23 +8,19 @@
           <v-select :items="['Dog', 'Cat', 'Bird']" v-model="answer" label="Image" required></v-select>
         </v-flex>
         <v-flex xs4 offset-xs1>
-          <v-btn
-            color="green darken-1"
-            v-show="!callback_success"
-            @click="send_response()"
-          >Send Answer</v-btn>
+          <v-btn color="green darken-1" @click="send_response()" >Send Answer</v-btn>
         </v-flex>
       </v-layout>
 
       <v-card-actions>
         <v-alert
-          :value="callback_success"
+          :value="server_result"
           type="success"
           icon="check_circle"
           outline
-        >Server Updated with with your response.</v-alert>
+          >{{ server_result }}</v-alert>
         <v-spacer></v-spacer>
-        <v-btn color="primary" v-show="callback_success" @click="dialog=false">Close</v-btn>
+        <v-btn color="primary" @click="dialog = false">Close</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -36,23 +32,23 @@ export default {
     SHOW_IMAGE: function(socket_data) {
       this.image = "data:image/jpeg;base64," + socket_data.buffer;
       this.dialog = true;
-      this.answer = '';
-      this.callback_success = false;
+      this.answer = "";
+      this.server_result = "";
     }
   },
   data: () => ({
     dialog: false,
     answer: "",
-    callback_success: false,
-    image: ""
+    image: "",
+    server_result: ""
   }),
+
   methods: {
     send_response() {
       const message_data = {};
       message_data.response = this.answer;
-      this.$socket.emit("QUIZ_RESPONSE", message_data, () => {
-        // Server has ack
-        this.callback_success = true;
+      this.$socket.emit("QUIZ_RESPONSE", message_data, result => {
+        this.server_result = result;
       });
     }
   }
